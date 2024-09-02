7aac562e6ad35b0c964d6bc9a23531c8e1a69fdf
Sardul Ojha <sardul.ojha@gmail.com>
2024-09-02 13:18:28 +0545

    simple changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sardulojha/Desktop/ChatBot/venv"
+set -gx VIRTUAL_ENV "/Users/sardulojha/Desktop/Github/PalmMind-Chatbot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
